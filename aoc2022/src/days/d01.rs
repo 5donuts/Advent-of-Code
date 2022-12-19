37cc28eb21028a886c9c64f664e81117b197f1ed
Charles German <5donuts@pm.me>
2022-12-19 13:27:41 -0500

    Add 2022 day 1

---
+++ aoc2022/src/days/d01.rs
@@ -0,0 +1,62 @@
+use utils::PuzzleResult;
+
+pub fn part1(input: &str) -> PuzzleResult {
+    let max_calories: u32 = input
+        .replace("\r", "")
+        .split("\n\n")
+        .map(|elf| {
+            elf.lines()
+                .map(|calories| calories.parse::<u32>().unwrap())
+                .sum()
+        })
+        .max()
+        .ok_or("Could not fetch max calories".to_string())?;
+
+    Ok(format!("{}", max_calories))
+}
+
+pub fn part2(input: &str) -> PuzzleResult {
+    let mut elf_calories: Vec<u32> = input
+        .replace("\r", "")
+        .split("\n\n")
+        .map(|elf| {
+            elf.lines()
+                .map(|calories| calories.parse::<u32>().unwrap())
+                .sum()
+        })
+        .collect();
+    elf_calories.sort_by(|a, b| b.cmp(a)); // reverse sort (i.e., descending)
+
+    let top_three_cals: u32 = elf_calories[..3].iter().sum();
+    Ok(format!("{}", top_three_cals))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &'static str = r#"1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"#;
+
+    #[test]
+    fn p1() {
+        assert_eq!(part1(INPUT).unwrap(), "24000".to_string());
+    }
+
+    #[test]
+    fn p2() {
+        assert_eq!(part2(INPUT).unwrap(), "45000".to_string());
+    }
+}
---
+++ aoc2022/src/main.rs
@@ -0,0 +1,15 @@
+mod days;
+use utils::Day;
+
+fn main() {
+    let days = vec![Day {
+        num: 1,
+        p1: days::d01::part1,
+        p2: days::d01::part2,
+        input: "aoc2022/input/1",
+    }];
+
+    for day in days {
+        day.run().unwrap();
+    }
+}
