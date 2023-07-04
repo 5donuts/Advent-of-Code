18771a4e8d8a572574fa6588577ec8d768dc2f15
Charles German <5donuts@pm.me>
2023-07-04 00:38:23 -0400

    Stop the suffering
    In debug mode, this takes ~40 seconds to run on my hardware (only a
    couple seconds in release mode, but 99% of the time I'm going to be
    running these puzzles in debug mode).

--- aoc2015/src/days/d04.rs
+++ aoc2015/src/days/d04.rs
@@ -1,36 +1,39 @@
+#[allow(unused_imports)]
 use md5::{Digest, Md5};
 use utils::PuzzleResult;
 
-pub fn part1(input: &str) -> PuzzleResult {
-    let secret_key = input.trim();
-    let mut hasher = Md5::new();
-    let mut answer: u32 = 0;
+pub fn part1(_input: &str) -> PuzzleResult {
+    // let secret_key = input.trim();
+    // let mut hasher = Md5::new();
+    // let mut answer: u32 = 0;
 
-    loop {
-        hasher.update(format!("{}{}", secret_key, answer));
-        let hash = hasher.finalize_reset();
-        // Check for at least 5 leading zeroes in the hexadecimal output
-        if hash[0] == 0 && hash[1] == 0 && hash[2] < 16 {
-            return Ok(format!("{}", answer));
-        }
-        answer += 1;
-    }
+    // loop {
+    //     hasher.update(format!("{}{}", secret_key, answer));
+    //     let hash = hasher.finalize_reset();
+    //     // Check for at least 5 leading zeroes in the hexadecimal output
+    //     if hash[0] == 0 && hash[1] == 0 && hash[2] < 16 {
+    //         return Ok(format!("{}", answer));
+    //     }
+    //     answer += 1;
+    // }
+    Ok("-- skipped --".into())
 }
 
-pub fn part2(input: &str) -> PuzzleResult {
-    let secret_key = input.trim();
-    let mut hasher = Md5::new();
-    let mut answer: u32 = 0;
+pub fn part2(_input: &str) -> PuzzleResult {
+    // let secret_key = input.trim();
+    // let mut hasher = Md5::new();
+    // let mut answer: u32 = 0;
 
-    loop {
-        hasher.update(format!("{}{}", secret_key, answer));
-        let hash = hasher.finalize_reset();
-        // Check for at least 6 leading zeroes in the hexadecimal output
-        if hash[0] == 0 && hash[1] == 0 && hash[2] == 0 {
-            return Ok(format!("{}", answer));
-        }
-        answer += 1;
-    }
+    // loop {
+    //     hasher.update(format!("{}{}", secret_key, answer));
+    //     let hash = hasher.finalize_reset();
+    //     // Check for at least 6 leading zeroes in the hexadecimal output
+    //     if hash[0] == 0 && hash[1] == 0 && hash[2] == 0 {
+    //         return Ok(format!("{}", answer));
+    //     }
+    //     answer += 1;
+    // }
+    Ok("-- skipped --".into())
 }
 
 #[cfg(test)]
